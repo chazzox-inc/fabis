78a3c780f9216980e12f1254a8c66345e0e8f540
chazzox <caylott@outlook.com>
2022-07-22 01:06:57 +0100

    fix: better text capitalisation

--- src/app.tsx
+++ src/app.tsx
@@ -2,11 +2,11 @@ const App = () => {
   return (
     <div className="h-screen flex items-center justify-center dark:bg-dark">
       <div className="shadow-main p-6 text-center mx-4 bg-white rounded-2xl">
-        <h2 className="text-2xl font-bold">fabis</h2>
-        <h3 className="text-xl my-2">
-          friends of acquired brain injury survivors
+        <h2 className="text-2xl font-bold">FABIS</h2>
+        <h3 className="text-xl my-2 font-semibold">
+          Friends of Acquired Brain Injury Survivors
         </h3>
-        <h3 className="text-lg my-2">Website Coming soon...</h3>
+        <h3 className="text-lg my-2 font-medium">Website Coming Soon...</h3>
       </div>
     </div>
   );
