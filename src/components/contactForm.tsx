3a5291ae75176a580a7d20031e10c7a059b7adcf
chazzox <caylott@outlook.com>
2022-12-24 00:35:51 +0000

    feat: adding solid contact form

---
+++ src/components/contactForm.tsx
@@ -0,0 +1,9 @@
+const ContactForm = () => {
+    return <>
+    <form action="" method="post">
+    <input type="text">test</input>
+        <input type="text">test2</input>
+        </form></>
+};
+
+export default ContactForm;
