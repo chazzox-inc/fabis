823e5962accce8c70aa19c2df81ad6d5e1350f75
chazzox <caylott@outlook.com>
2022-12-24 01:13:35 +0000

    feat: added basic form structure

--- src/components/contactForm.tsx
+++ src/components/contactForm.tsx
@@ -1,9 +1,28 @@
 const ContactForm = () => {
-    return <>
-    <form action="" method="post">
-    <input type="text">test</input>
-        <input type="text">test2</input>
-        </form></>
+    return   (<form action="" method="post" class="flex flex-col max-w-3xl mx-auto items-center gap-4">
+    <div class="form-control w-full max-w-lg">
+        <label class="label">
+            <span class="label-text">Where can we contact you?</span>
+        </label>
+        <input type="text" placeholder="email@example.com" class="input input-bordered w-full max-w-lg" />
+    </div>
+    <div class="form-control w-full max-w-lg">
+        <label class="label">
+            <span class="label-text">What is the subject?</span>
+        </label>
+        <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-lg" />
+    </div>
+    
+    <div class="form-control w-full max-w-lg">
+        <label class="label">
+            <span class="label-text">What is your message?</span>
+        </label>
+        <textarea class="textarea" placeholder="Bio"></textarea>
+    </div>
+
+    <input type="submit" value="Submit" class="btn btn-primary" />
+       
+        </form> )
 };
 
 export default ContactForm;
