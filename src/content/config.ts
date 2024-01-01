8eb9baa232009ff2cadcbe6c28ac1120941a7d96
charlie.aylott <charlie.aylott@matillion.com>
2024-01-01 21:42:03 +0000

    feat: added press

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,6 @@
 import { defineCollection } from "astro:content";
 import z from "zod";
-// 2. Define your collection(s)
+
 const blogCollection = defineCollection({
     schema: z.object({
         title: z.string()
