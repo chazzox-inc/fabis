ff2ce0acecbf94559e6e4985d684656c4fe14928
charlie.aylott <charlie.aylott@matillion.com>
2024-10-15 19:08:01 +0100

    chore: updated everything, added new page

--- src/pages/api/contact-form.ts
+++ src/pages/api/contact-form.ts
@@ -1,6 +1,6 @@
 import type { APIRoute } from "astro";
-import type { Email } from "node-mailjet";
 import zod from "zod";
+import { Resend } from "resend";
 
 export const post_schema = zod.object({
     email: zod.string().email(),
@@ -9,65 +9,44 @@ export const post_schema = zod.object({
     message: zod.string().min(3)
 });
 
-export const post: APIRoute = async ({ request }) => {
+export const POST: APIRoute = async ({ request }) => {
     const data_unsafe = await request.json();
 
     // validate input data
-    const data = post_schema.safeParse(data_unsafe);
+    const { success, data, error } = post_schema.safeParse(data_unsafe);
 
     // validation unsuccessful
-    if (!data.success)
-        return new Response(
-            JSON.stringify({ success: false, message: data.error }),
-            { status: 504 }
-        );
+    if (!success)
+        return new Response(JSON.stringify({ success: false, message: error }), {
+            status: 504
+        });
 
-    const body_data = data.data;
+    // send resend email
+    const resend = new Resend(import.meta.env.RESEND_API_KEY);
 
-    // send mailjet email
-    const req = await fetch("https://api.mailjet.com/v3.1/send", {
-        body: JSON.stringify({
-            Messages: [
-                {
-                    From: {
-                        Email: "contact-form@chazzox.uk",
-                        Name: "Landing page contact form"
-                    },
-                    To: [
-                        {
-                            Email: "admin@fabischarity.com",
-                            Name: "FABIS CONTACT US FORM"
-                        },
-                        {
-                            Email: "caylott@outlook.com",
-                            Name: "FABIS CONTACT US FORM"
-                        }
-                    ],
-                    Subject: `User Contact Submission: ${body_data.subject}`,
-                    TextPart: `This is an automated message: \nUser name: ${body_data.name}\nUser email ${body_data.email}\n\nUser Message: ${data.data.message}`
-                }
-            ]
-        }),
-        headers: {
-            Authorization: `Basic ${btoa(
-                `${import.meta.env.SECRET_MAILJET_API_ID}:${
-                    import.meta.env.SECRET_MAILJET_API_SECRET
-                }`
-            )}`,
-            "Content-Type": "application/json"
-        },
-        method: "POST"
+    const { data: resendData, error: resendError } = await resend.emails.send({
+        from: "Landing page contact form <contact-form@chazzox.uk>",
+        to: [
+            "FABIS CONTACT US FORM <admin@fabischarity.com>",
+            "FABIS CONTACT US FORM <caylott@outlook.com>"
+        ],
+        subject: `User Contact Submission: ${data.subject}`,
+        text: `This is an automated message: \nUser name: ${data.name}\nUser email ${data.email}\n\nUser Message: ${data.message}`
     });
-    const res = (await req.json()) as Email.PostResponseData;
 
-    const statusMessageReduced = res.Messages.map((v) => v.Status).reduce((a, b) =>
-        a == "success" && b == a ? a : b
-    );
+    if (resendError) {
+        return new Response(
+            JSON.stringify({
+                success: false,
+                message: resendError.message
+            })
+        );
+    }
 
-    return {
-        body: JSON.stringify({
-            message: res.Messages[0]?.Status,
-            success: statusMessageReduced == "success"
+    return new Response(
+        JSON.stringify({
+            success: true,
+            message: resendData
         })
-    };
+    );
 };
